2d1e389c0f5ae0843987fd691ce1e82d6ffe1b03
Paul Cobbaut <paul@cobbaut.be>
2022-07-06 11:19:57 +0200

    six dots

--- braille.py
+++ braille.py
@@ -30,8 +30,14 @@ dot.Angle1 = -90
 dot.Angle2 = 90
 dot.Angle3 = 180
 dot.Placement = FreeCAD.Placement(Vector(0, 0, 0), FreeCAD.Rotation(180, 0, 90))
+dot.ViewObject.hide()
 
+# positions of braille dots
+14
+25
+36
 
+# braille alphabet
 """
 a 	⠁ 	1
 b 	⠃ 	12
@@ -65,22 +71,67 @@ braille = {
   "a" : "1",
   "b" : "12",
   "c" : "14",
-  "d" : "145"
+  "d" : "145",
+  "e" : "15"
 }
 
 
 doc.recompute()
 
 # place a dot in 1
-def place_a_dot():
+def place_a_dot_in_1():
     obj = doc.addObject('Part::Feature','dot')
     obj.Shape = doc.dot.Shape
-    obj.Placement = FreeCAD.Placement(Vector(0, 0, 0), FreeCAD.Rotation(180,0,90), Vector(0,0,0))
+    obj.Label = "dot1"
+    position = FreeCAD.Vector(0,100,0)
+    rotation = FreeCAD.Rotation(180,0,90)
+    obj.Placement = FreeCAD.Placement(position, rotation)
 
 # place a dot in 2
 def place_a_dot_in_2():
     obj = doc.addObject('Part::Feature','dot')
     obj.Shape = doc.dot.Shape
+    obj.Label = "dot2"
+    position = FreeCAD.Vector(0,50,0)
+    rotation = FreeCAD.Rotation(180,0,90)
+    obj.Placement = FreeCAD.Placement(position, rotation)
+
+
+# place a dot in 3
+def place_a_dot_in_3():
+    obj = doc.addObject('Part::Feature','dot')
+    obj.Shape = doc.dot.Shape
+    obj.Label = "dot3"
+    position = FreeCAD.Vector(0,0,0)
+    rotation = FreeCAD.Rotation(180,0,90)
+    obj.Placement = FreeCAD.Placement(position, rotation)
+
+
+# place a dot in 4
+def place_a_dot_in_4():
+    obj = doc.addObject('Part::Feature','dot')
+    obj.Shape = doc.dot.Shape
+    obj.Label = "dot4"
+    position = FreeCAD.Vector(50,100,0)
+    rotation = FreeCAD.Rotation(180,0,90)
+    obj.Placement = FreeCAD.Placement(position, rotation)
+
+
+# place a dot in 5
+def place_a_dot_in_5():
+    obj = doc.addObject('Part::Feature','dot')
+    obj.Shape = doc.dot.Shape
+    obj.Label = "dot5"
+    position = FreeCAD.Vector(50,50,0)
+    rotation = FreeCAD.Rotation(180,0,90)
+    obj.Placement = FreeCAD.Placement(position, rotation)
+
+
+# place a dot in 6
+def place_a_dot_in_6():
+    obj = doc.addObject('Part::Feature','dot')
+    obj.Shape = doc.dot.Shape
+    obj.Label = "dot6"
     position = FreeCAD.Vector(50,0,0)
     rotation = FreeCAD.Rotation(180,0,90)
     obj.Placement = FreeCAD.Placement(position, rotation)
@@ -88,25 +139,23 @@ def place_a_dot_in_2():
 
 def print_braille_character(char):
     if "1" in braille[char]:
-        print("1 erin")
+        place_a_dot_in_1()
     if "2" in braille[char]:
-        print("2 erin")
         place_a_dot_in_2()
     if "3" in braille[char]:
-        print("3 erin")
+        place_a_dot_in_3()
     if "4" in braille[char]:
-        print("4 erin")
+        place_a_dot_in_4()
     if "5" in braille[char]:
-        print("5 erin")
-        place_a_dot()
+        place_a_dot_in_5()
     if "6" in braille[char]:
-        print("6 erin")
+        place_a_dot_in_6()
 
-print("test a")
-print_braille_character("a")
+#print("test a")
+#print_braille_character("a")
 
-print("test b")
-print_braille_character("b")
+#print("test b")
+#print_braille_character("b")
 
 print("test d")
 print_braille_character("d")
