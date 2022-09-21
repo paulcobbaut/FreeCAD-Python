af4391d7656684581fd08e9c0428725ebfb08296
Paul Cobbaut <paul@cobbaut.be>
2022-09-21 20:53:03 +0200

    trying out different sizes and spacing for braille dots

--- dovetail.py
+++ dovetail.py
@@ -105,20 +105,13 @@ dove_length = piece_length / 40
 dove_hyp = 2 * dove_length       # Hypotenuse at 30 degree angle
 dove_side = 1.732 * dove_length  # other side at 30 degree angle
 
-
-
-
+# create a sketch with dovetails and pockets that will be padded later
 sketch = doc.getObject('Body').newObject("Sketcher::SketchObject", "Sketch")
 
 
 
-#addGeometry(Part.LineSegment(App.Vector(0, 0, 0), App.Vector(s, w, 0)), False)
-#sketch.addGeometry(Part.LineSegment(App.Vector(s, w, 0), App.Vector(s + l, w, 0)), False)
-#sketch.addGeometry(Part.LineSegment(App.Vector(s + l, w, 0), App.Vector(s + s + l, 0, 0)), False)
-#sketch.addGeometry(Part.LineSegment(App.Vector(s + s + l, 0, 0), App.Vector(0, 0, 0)), False)
-
-
 # start drawing piece at origin
+# first part draws left side
 x = 0
 y = (piece_length / 4) - (dove_width / 2)
 sketch.addGeometry(Part.LineSegment(App.Vector(0, 0, 0), App.Vector(x, y, 0)), False)
@@ -175,20 +168,19 @@ x = 0
 y = piece_length
 sketch.addGeometry(Part.LineSegment(App.Vector(px, py, 0), App.Vector(x, y, 0)), False)
 
-# copy and place at opposite side
+
+# copy complete left side and place at opposite side
 doc.getObject('Sketch').addCopy([0,1,2,3,4,5,6,7,8], App.Vector(piece_length, 0, 0), True)
 
 
 
-
-# bottom 
-
 # start drawing piece at origin
+# first part draws bottom side
 x = 0
 y = (piece_length / 4) - (dove_width / 2)
 sketch.addGeometry(Part.LineSegment(App.Vector(0, 0, 0), App.Vector(y, x, 0)), False)
 
-# left_bottom pocket
+# bottom_left dovetail
 px = x
 py = y
 x = dove_length
@@ -214,7 +206,7 @@ x = 0
 y = (piece_length * 3 / 4) - (dove_width / 2)
 sketch.addGeometry(Part.LineSegment(App.Vector(py, -px, 0), App.Vector(y, -x, 0)), False)
 
-# left_top dovetail
+# bottom_right pocket
 px = x
 py = y
 x = 0 - dove_length
@@ -233,27 +225,258 @@ x = 0
 y = (piece_length * 3 / 4) - (dove_width / 2) + dove_width
 sketch.addGeometry(Part.LineSegment(App.Vector(py, -px, 0), App.Vector(y, -x, 0)), False)
 
-# side to top
+# side to far right
 px = x
 py = y
 x = 0
 y = piece_length
 sketch.addGeometry(Part.LineSegment(App.Vector(py, -px, 0), App.Vector(y, -x, 0)), False)
 
-# copy and place at opposite side
+# copy and place at opposite (top) side
 doc.getObject('Sketch').addCopy([18,19,20,21,22,23,24,25,26], App.Vector(0, piece_length, 0), True)
 
 
-
+# pad this sketch
 pad = doc.getObject('Body').newObject("PartDesign::Pad", "Pad")
 pad.Profile = doc.getObject("Sketch")
-pad.Length = 10
+pad.Length = 2
+
+#
+# we now have an empty piece with dovetails and pockets that fits with itself
+#
+
+# start putting braille on it
+
+
+
+"""
+braille.py
+Paul Cobbaut, 2022-07-05
+FreeCAD Braille font
+This script parses text and converts it to Braille dots in FreeCAD
+"""
+
+
+# position of Braille dots
+"""
+14
+25
+36
+"""
+
+
+# Braille alphabet
+# List of supported characters
+"""
+a 	⠁ 	1
+b 	⠃ 	12
+c 	⠉ 	14
+d 	⠙ 	145
+e 	⠑ 	15
+f 	⠋ 	124
+g 	⠛ 	1245
+h 	⠓ 	125
+i 	⠊ 	24
+j 	⠚ 	245
+k 	⠅ 	13
+l 	⠇ 	123
+m 	⠍ 	134
+n 	⠝ 	1345
+o 	⠕ 	135
+p 	⠏ 	1234
+q 	⠟ 	12345
+r 	⠗ 	1235
+s 	⠎ 	234
+t 	⠞ 	2345
+u 	⠥ 	136
+v 	⠧ 	1236
+w 	⠺ 	2456
+x 	⠭ 	1346
+y 	⠽ 	13456
+z 	⠵ 	1356
+number indicator 	⠼ 	3456
+1 	⠼⠁ 	3456 1
+2 	⠼⠃ 	3456 12
+3 	⠼⠉ 	3456 14
+4 	⠼⠙ 	3456 145
+5 	⠼⠑ 	3456 15
+6 	⠼⠋ 	3456 124
+7 	⠼⠛ 	3456 1245
+8 	⠼⠓ 	3456 125
+9 	⠼⠊ 	3456 24
+0 	⠼⠚ 	3456 245
+"""
+
+
+# dictionary character : dots printed
+braille = {
+  "a" : "1",
+  "b" : "12",
+  "c" : "14",
+  "d" : "145",
+  "e" : "15",
+  "f" : "124",
+  "g" : "1245",
+  "h" : "125",
+  "i" : "24",
+  "j" : "245",
+  "k" : "13",
+  "l" : "123",
+  "m" : "134",
+  "n" : "1345",
+  "o" : "135",
+  "p" : "1234",
+  "q" : "12345",
+  "r" : "1235",
+  "s" : "234",
+  "t" : "2345",
+  "u" : "136",
+  "v" : "1236",
+  "w" : "2456",
+  "x" : "1346",
+  "y" : "13456",
+  "z" : "1356",
+  "1" : "1",
+  "2" : "12",
+  "3" : "14",
+  "4" : "145",
+  "5" : "15",
+  "6" : "124",
+  "7" : "1245",
+  "8" : "125",
+  "9" : "24",
+  "0" : "245",
+  " " : ""
+}
+
+# These four define the size of the dots in mmm
+dot_size = .5          # diameter of a dot
+dot_separation = 2.5   # space between center of dots
+char_separation = 6.4  # space between center of characters
+line_separation = 32   # space between center of lines
+
+
+# create the template dot that is always copied
+def create_template_dot(dotname):
+    tdot = doc.addObject("Part::Sphere", dotname)
+    tdot.Radius = dot_size
+    tdot.Angle1 = -90
+    tdot.Angle2 = 90
+    tdot.Angle3 = 180
+    tdot.Placement = FreeCAD.Placement(Vector(0, 0, 0), FreeCAD.Rotation(180, 0, 90))
+    tdot.ViewObject.hide()
+    doc.recompute()	# This seems needed, otherwise nothing appears in FreeCAD
+
+
+# place a dot in the correct position
+def place_a_dot(dot_number, char_count, line_count, dotname):		# dot_number is Braille dot 1, 2, 3, 4, 5 or 6
+    obj = doc.addObject('Part::Feature', dotname)			# copy the template dot
+    obj.Shape = doc.getObject(dotname).Shape
+    obj.Label = "dot_" + str(line_count) + "_" + str(char_count) + "_" + str(dot_number)	# name has 'some' meaning: dot + character position + Braille dot
+    # Get X coordinate
+    left_right = dot_number >> 2				# 0 if 1,2 or 3, 1 if 4, 5 or 6 (zero means dot on the left, one means dot on the right)
+    char_position = char_count * char_separation		# this is the n-th character (n = char_count + 1 )
+    dot_position = dot_separation * left_right                  # this is either a 123 dot on the left, or a 456 dot on the right
+    x = char_position + dot_position
+    # Get Y coordinate
+    line_position = line_separation * line_count
+    y = dot_separation * (- dot_number % 3) - line_position	# negative modulo 3 gives Y coordinate (three dots above each other)
+    # Finale position for this dot
+    pad_height = 2
+    position = FreeCAD.Vector(x, y, pad_height)
+    rotation = FreeCAD.Rotation(180, 0, 90)
+    obj.Placement = FreeCAD.Placement(position, rotation)	# put copied and named dot in correct location
+    return obj
+
+
+# prints one Braille string
+def print_braille_string(string, line_count, dotname):
+    # keeps track of the n-th character on each line
+    # is used for the position of the current character
+    char_count = 0
+    # list te create compound when string is written
+    compound_list = []
+    # when this is True, then put no 'Number Indicator' in Braille
+    previous_is_digit = False
+    for letter in string:
+        char_count = char_count + 1
+        if letter.isdigit():
+            if previous_is_digit == False:
+                previous_is_digit = True
+                # Braille Number Indicator = "3456"
+                obj = place_a_dot(3, char_count, line_count, dotname)
+                compound_list.append(obj)
+                obj = place_a_dot(4, char_count, line_count, dotname)
+                compound_list.append(obj)
+                obj = place_a_dot(5, char_count, line_count, dotname)
+                compound_list.append(obj)
+                obj = place_a_dot(6, char_count, line_count, dotname)
+                compound_list.append(obj)
+                char_count = char_count + 1
+        else:
+            previous_is_digit = False
+        for i in range(1,7):
+            if str(i) in braille[letter]:
+                obj = place_a_dot(i, char_count, line_count, dotname)
+                compound_list.append(obj)
+    line_name = "line_" + str(line_count)
+    obj = doc.addObject("Part::Compound",line_name)
+    obj.Links = compound_list
+    doc.recompute()	# This seems needed, otherwise nothing appears in FreeCAD
+
+
+# program starts here
+def main():
+    global dot_size
+    global dot_separation
+    global char_separation
+    global line_separation
+
+    # line_count keeps track of the n-th line
+    # is used for the position of the current line
+    line_count = 0
+
+    create_template_dot("dot1")
+    print_braille_string("abcdefghijklmnopqrstuvwxyz", line_count, "dot1")
+    line_count = line_count + 1
+
+    # change dot values
+    # These four define the size of the dots in mmm
+    dot_size = 2          # diameter of a dot
+    dot_separation = 8   # space between center of dots
+    char_separation = 10  # space between center of characters
+    line_separation = 20   # space between center of lines
+
+    # re-create dot with different values
+    create_template_dot("dot2")
+    print_braille_string("dit is braille 1 en 2 en 42", line_count, "dot2")
+    line_count = line_count + 1
+
+    # dot3
+    dot_size = 3          # diameter of a dot
+    dot_separation = 8   # space between center of dots
+    char_separation = 10  # space between center of characters
+    line_separation = 20   # space between center of lines
+    create_template_dot("dot3")
+    print_braille_string("alea jacta est", line_count, "dot3")
+    line_count = line_count + 1
+
+
+    # dot4
+    dot_size = .3          # diameter of a dot
+    dot_separation = 4   # space between center of dots
+    char_separation = 6  # space between center of characters
+    line_separation = 20   # space between center of lines
+    create_template_dot("dot4")
+    print_braille_string("2 4 8 16 32 64 128 256", line_count, "dot4")
+
+
+main()
+
 
 
 
 doc.recompute()
 
-
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
-
 
