9b0503f9810a8fba90ad7721a24124f1ec57e4d7
Paul Cobbaut <paul@cobbaut.be>
2022-09-23 22:46:39 +0200

    yes custom brick... sort of

--- brick_freecad.py
+++ brick_freecad.py
@@ -1,12 +1,8 @@
 """
-lego_FreeCAD.py
-Paul Cobbaut, 2022-09-23
-Some FreeCAD scripting
+brick_FreeCAD.py -- Paul Cobbaut, 2022-09-23
 The goal is to make nonexisting Lego-compatible pieces for use in 3D printer
 For example a 30x10 studs box with flat bottom, 8 bricks high, studs all over the top side
 """
-
-
 # Dimensions used
 stud_radius = 2.4		# documented as 2.4 or 2.5 or 2.47
 stud_center_spacing = 8.0
@@ -20,11 +16,6 @@ brick_height = 9.6		# stud_center_spacing * 6 / 5  (or plate_height * 3)
 brick_wall  = 0.75		# (stud_center_spacing - stud_diameter) / 2 / 2
 brick_width = 7.8		# = plate_width
 
-# open underside from onionrobots
-#underside_cylinder_outer_diameter = 6.31
-#underside_cylinder_wall_thickness = 0.657
-
-
 import FreeCAD
 from FreeCAD import Base, Vector
 import Part
@@ -33,6 +24,10 @@ import Sketcher
 # FreeCAD document
 doc = FreeCAD.newDocument("Lego brick generated")
 obj = doc.addObject("PartDesign::Body", "Body")
+
+def calculate_width(y):
+    w = (y * (brick_width + gap)) - gap
+    return w
 
 def make_prism(name, x, y, z):
     obj = doc.addObject("Part::Box", name)
@@ -49,34 +44,30 @@ def make_stud(name):
     doc.recompute()
     return obj
 
-def calculate_width(y):
-    w = (y * (brick_width + gap)) - gap
-    return w
-
 stud_template = make_stud("stud_template")
+stud_template.ViewObject.hide()
 
+def create_studs(name, x, y, z):
+    for i in range(int(x)):
+        for j in range(int(y)):
+            obj = doc.addObject('Part::Feature','stud_template')
+            obj.Shape = doc.stud_template.Shape
+            obj.Label = name + str(i) + '__' + str(j)
+            xpos = ((i+1) * stud_center_spacing) - (stud_center_spacing / 2)
+            ypos = ((j+1) * stud_center_spacing) - (stud_center_spacing / 2)
+            obj.Placement = FreeCAD.Placement(Vector(xpos, ypos, z), FreeCAD.Rotation(0,0,0), Vector(0,0,0))
 
-# create a 1x1 brick
-brick1 = make_prism("brick1x1", brick_width, brick_width, brick_height)
-doc.brick1x1.Placement = FreeCAD.Placement(Vector(0, 0, 0), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
-stud1 = make_stud("stud1x1")
-doc.stud1x1.Placement = FreeCAD.Placement(Vector(0, 0, brick_height), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
+def create_a_brick(xstuds, ystuds):
+    width = calculate_width(xstuds)
+    length = calculate_width(ystuds)
+    prism = make_prism("prism", width, length, brick_height)
+    studs = create_studs("studs", xstuds, ystuds, brick_height)
 
-
-# create an x by y brick
-x = 2
-y = 6
-length = calculate_width(y)
-width = calculate_width(x)
-prism = make_prism("prism", width, length, brick_height)
-#studs = create_studs(width, length)
+create_a_brick(2, 6)
 
 
 
 doc.recompute()
 
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
-
-
-
 
