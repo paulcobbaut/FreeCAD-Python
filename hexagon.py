d5ba2f049306407103b7ea21e9f9ebe9ef27b2d2
Paul Cobbaut <paul@cobbaut.be>
2022-10-07 18:44:09 +0200

    finished the hexagon program

--- hexagon.py
+++ hexagon.py
@@ -2,7 +2,8 @@
 hexagon.py
 Paul Cobbaut, 2022-10-07
 Some FreeCAD scripting
-The goal is to make a hexagon pattern for pocketing.
+The goal is to make a hexagon pattern of a fixed size for pocketing.
+It works, finished!
 """
 
 import FreeCAD
@@ -16,27 +17,31 @@ doc = FreeCAD.newDocument("Hexagon scripted")
 obj = doc.addObject("PartDesign::Body", "Body")
 
 # hexagon size
-hex_radius_mm = 3
-hex_sides_mm = 0.866 * hex_radius_mm
-hex_gap_mm = hex_radius_mm / 3
+hex_radius_mm	= 3
+hex_cevian_mm	= 0.866 * hex_radius_mm
+gap_mm		= hex_radius_mm / 3
 
 # create a sketch 
 sketch = doc.getObject('Body').newObject("Sketcher::SketchObject", "Sketch")
 
-
-#ProfileLib.RegularPolygon.makeRegularPolygon(doc.getObject('Sketch'),6,App.Vector(0,0,0),App.Vector(hex_sides_mm,hex_radius_mm/2,0),False)
-
-
 def create_hexagon_grid(x_mm, y_mm):
-    width = 2 * hex_radius_mm + hex_gap_mm
-    x_count = int(x_mm / width)
-    for i in range(x_count):
-        ProfileLib.RegularPolygon.makeRegularPolygon(doc.getObject('Sketch'),6,App.Vector(i * width, 0, 0),App.Vector(hex_sides_mm + i*width,hex_radius_mm/2,0),False)
+    space_mm = 2 * hex_radius_mm + gap_mm  # space needed by a hex and its empty space around it
+    x_count = int(x_mm / space_mm)
+    y_count = int(y_mm / space_mm)
+    for j in range(y_count):
+        o_y = j * space_mm
+        r_y = j * space_mm + hex_radius_mm/2
+        for i in range(x_count):
+            if (j % 2) == 0:
+                o_x = i * space_mm
+                r_x = i * space_mm + hex_cevian_mm
+            else:
+                o_x = i * space_mm + space_mm/2
+                r_x = i * space_mm + hex_cevian_mm + space_mm/2
+            ProfileLib.RegularPolygon.makeRegularPolygon(doc.getObject('Sketch'),6,App.Vector(o_x, o_y, 0),App.Vector(r_x, r_y, 0),False)
     
 
 create_hexagon_grid(100, 50)
 
-
 doc.recompute()
-
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
