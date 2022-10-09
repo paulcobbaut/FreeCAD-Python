40263f59bed39523085c8bc7c4d797ef48f85789
Paul Cobbaut <paul@cobbaut.be>
2022-10-09 17:06:55 +0200

    hexagon.py finished some more :)

--- hexagon.py
+++ hexagon.py
@@ -25,23 +25,24 @@ gap_mm		= hex_radius_mm / 3
 sketch = doc.getObject('Body').newObject("Sketcher::SketchObject", "Sketch")
 
 def create_hexagon_grid(x_mm, y_mm):
-    space_mm = 2 * hex_radius_mm + gap_mm  # space needed by a hex and its empty space around it
-    x_count = int(x_mm / space_mm)
-    y_count = int(y_mm / space_mm)
+    x_space_mm = 2 * hex_cevian_mm + gap_mm  # space needed by a hex and its empty space in the X-axis
+    y_space_mm = 1.5 * hex_radius_mm + gap_mm  # space needed by a hex and its empty space in the X-axis	
+    x_count = int(x_mm / x_space_mm)
+    y_count = int(y_mm / y_space_mm)
     for j in range(y_count):
-        o_y = j * space_mm
-        r_y = j * space_mm + hex_radius_mm/2
+        o_y = j * y_space_mm
+        r_y = j * y_space_mm + hex_radius_mm/2
         for i in range(x_count):
             if (j % 2) == 0:
-                o_x = i * space_mm
-                r_x = i * space_mm + hex_cevian_mm
+                o_x = i * x_space_mm
+                r_x = i * x_space_mm + hex_cevian_mm
             else:
-                o_x = i * space_mm + space_mm/2
-                r_x = i * space_mm + hex_cevian_mm + space_mm/2
+                o_x = i * x_space_mm + x_space_mm/2
+                r_x = i * x_space_mm + hex_cevian_mm + x_space_mm/2
             ProfileLib.RegularPolygon.makeRegularPolygon(doc.getObject('Sketch'),6,App.Vector(o_x, o_y, 0),App.Vector(r_x, r_y, 0),False)
     
 
-create_hexagon_grid(100, 50)
+create_hexagon_grid(50, 30)
 
 doc.recompute()
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
