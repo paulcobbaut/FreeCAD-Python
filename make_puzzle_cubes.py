2f04c6ba1c0f108ea91a063b5122e8dd278bfe04
Paul Cobbaut <paul@cobbaut.be>
2024-07-18 23:18:21 +0200

    works for even sizes to generate printable cube puzzles

--- make_puzzle_cubes.py
+++ make_puzzle_cubes.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 #make_puzzle_cubes.py -- Paul Cobbaut, 2024-07-01
-#the part of the code without FreeCAD
 
 import FreeCAD
 from FreeCAD import Base, Vector
 import Part
 import Mesh
+import random
 
-size   = 3                                                                         # cube size
+size   = 12                                                                         # cube size
 full   = [[[1 for k in range(size)] for j in range(size)] for i in range(size)]    # full cube
 empty  = [[[0 for k in range(size)] for j in range(size)] for i in range(size)]    # empty cube
 pieces = {}                                                                        # dict for all pieces
-
 unitmm = 10  # size of a 1x1x1 3D printed cube
 
 # FreeCAD document
@@ -45,6 +44,9 @@ def move_row(fromcube, pieceindex, x, y):
     for z in range(size):
         move(fromcube, pieces["blk{0}".format(pieceindex)], x, y, z)
 
+def move_unit(fromcube, pieceindex, x, y, z):
+        move(fromcube, pieces["blk{0}".format(pieceindex)], x, y, z)
+
 def makeunitcube(x, y, z):
     obj        = doc.addObject("Part::Box","Box")
     obj.Label  = "Box"
@@ -70,32 +72,69 @@ def pieces_to_3mf(pieces):
         for x, X in enumerate(piece):
             for y, Y in enumerate(X):
                 for z, Z in enumerate(Y):
-                    print(Z)
                     if Z == 1:
                         piecepart = makeunitcube(x,y,z)
                         compound_piece.append(piecepart)
         make_3mf(piecename, compound_piece)
 
+def unit_still_in_full(x, y, z):
+    if full[x][y][z] == 1:
+        return True
+    return False
 
-# creates straight pieces to fill the cube
-i = 0
-for x in range(size):
-    for y in range(size):
-        pieces["blk{0}".format(i)] = [[[0 for k in range(size)] for j in range(size)] for i in range(size)]
-        move_row(full, i, x, y)
-        i += 1
+def move_unit_to_adjacent(x, y, z):
+    # find current adjacent pieces
+    adj = []
+    for piecename, piece in pieces.items():
+        if x >= 1:
+            tx = x - 1
+        else:
+            tx = x
+        if piece[tx][y][z] == 1:
+            adj.append(piecename)
+        if x == size - 1:
+            tx = x
+        else:
+            tx = x + 1
+        if piece[tx][y][z] == 1:
+            adj.append(piecename)
+        if y >= 1:
+            ty = y - 1
+        else:
+            ty = y
+        if piece[x][ty][z] == 1:
+            adj.append(piecename)
+        if y == size - 1:
+            ty = y
+        else:
+            ty = y + 1
+        if piece[x][ty][z] == 1:
+            adj.append(piecename)
+    print('adjacent are ' + str(adj))
+    # choose one of the adjacent pieces and attach to it
+    move_unit(full, random.choice(adj)[3:], x, y, z)
+    return
 
-'''
 # creates straight pieces but skips one every time
+skip = True
 i = 0
 for x in range(size):
     for y in range(size):
         pieces["blk{0}".format(i)] = [[[0 for k in range(size)] for j in range(size)] for i in range(size)]
-        move_row(full, i, x, y)
+        if skip:
+            move_row(full, i, x, y)
+            skip = False
+        else:
+            skip = True
         i += 1
-'''
 
-print_all_pieces()
+# second round
+for x in range(size):
+    for y in range(size):
+        for z in range(size):
+            if unit_still_in_full(x,y,z):
+                move_unit_to_adjacent(x,y,z)
+
+#print_all_pieces()
 pieces_to_3mf(pieces)
-
 
